dbef03ebe048e5217077cb2e103a082047fd785c
Maksim T <verdo.ya.ry19@gmail.com>
2023-06-14 22:54:11 +0400

    initial commit

---
+++ src/test/java/RestCalcTest.java
@@ -0,0 +1,20 @@
+package ru.netology.cicles;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import ru.netology.maven.RestCalc;
+
+public class RestCalcTest {
+
+    @Test
+    public void dayOfRest() {
+        int income = 100000;
+        int expense = 60000;
+        int thresold = 150000;
+        RestCalc service = new RestCalc();
+        int actual = service.restMonth(income, expense, thresold);
+        int expected = 2;
+        Assertions.assertEquals(expected, actual);
+
+
+    }
+}
